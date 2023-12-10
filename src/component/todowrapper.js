c5bd24ceb39ba5ec6aaf8ecaf2e86c84b76c3962
Huu Trung <huutrungdoan22202@gmail.com>
2023-12-11 01:12:31 +0700

    input item to form and display it

--- src/component/todowrapper.js
+++ src/component/todowrapper.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 import { TodoForm } from "./todoform";
+import { Todo } from "./todo";
 
 export const TodoWrapper = () => {
   const [todos, setTodos] = useState([]);
 
   const addTodo = (todo) => {
-    setTodos([...todos, { task: todo}]);
+    setTodos([...todos, { task: todo }]);
     console.log(todos);
   };
   return (
     <div className="TodoWrapper">
       <TodoForm addTodo={addTodo} />
+      {todos.map((todo, index) => (
+        <Todo task={todo} key={index} />
+      ))}
     </div>
   );
 };
