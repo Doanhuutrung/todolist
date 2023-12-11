8eb1e0323af33835d8bd2506acf269ece0c52be4
Huu Trung <huutrungdoan22202@gmail.com>
2023-12-11 10:15:09 +0700

    add function edit form

--- src/component/todowrapper.js
+++ src/component/todowrapper.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { TodoForm } from "./todoform";
 import { Todo } from "./todo";
+import { EditTodoForm } from "./editform";
 
 export const TodoWrapper = () => {
   const [todos, setTodos] = useState([]);
@@ -9,12 +10,47 @@ export const TodoWrapper = () => {
     setTodos([...todos, { task: todo }]);
     console.log(todos);
   };
+
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todos, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  const toggleCompleted = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
   return (
     <div className="TodoWrapper">
+      <h1>Get Things Done !</h1>
       <TodoForm addTodo={addTodo} />
-      {todos.map((todo, index) => (
-        <Todo task={todo} key={index} />
-      ))}
+      {/* display todos */}
+      {todos.map((todo) =>
+        todo.isEditing ? (
+          <EditTodoForm
+            editTodo={editTask}
+            task={todo}
+            toggleCompleted={toggleCompleted}
+          />
+        ) : (
+          <Todo key={todo.id} task={todo} editTodo={editTodo} />
+        )
+      )}
     </div>
   );
 };
